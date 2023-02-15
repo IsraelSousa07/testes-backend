0115760e8df13483751c42df32c80e3f4e98081d
IsraelSousa07 <49081086+IsraelSousa07@users.noreply.github.com>
2023-02-14 21:45:36 -0300

    Delete src directory

--- src/main/resources/migration/V1_0__create_table_unidade.sql
+++
@@ -1,6 +0,0 @@
-select * from tbl_unidade;CREATE TABLE `tbl_unidade` (
-`id_unidade` integer primary key auto_increment,
-`nome_unidade` varchar(100) NOT NULL,
-`endereco_unidade` varchar(100) NOT NULL,
-`telefone_unidade` varchar(100) NOT NULL,
-)
